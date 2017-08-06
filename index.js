3665854c705f9eb1aa8cc6690520d0aae347a494
Stephen Chung <spchung@andrew.cmu.edu>
2017-08-06 14:47:52 -0400

    Add CLI arguments to filter markets and designers to scrape

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+/* ---------------------------------------------------------------------------*/
 var fs = require('fs');
 var casper = require('casper').create({
     logLevel: 'debug'
 });
 
-var NUMSCROLLS = 250
+var NUMSCROLLS = 2
+
+var MARKETS = ['grailed', 'hype', 'core']
+
+var MARKETS_TO_SCRAPE = [] /* if empty, scrape all markets */
+var DESIGNERS_TO_SCRAPE = [] /* if empty, scrape all designers */
+/* ---------------------------------------------------------------------------*/
 var scrollNum = 0
 
-var loadFeed = function (numScrolls) {
-    casper.repeat(numScrolls, function () {
-        casper.scrollToBottom();
-        casper.wait(1000, function () {
-            casper.log('SCROLL: ' + ++scrollNum);
-        });
-    })
-}
-
 casper.start('https://grailed.com/', function() {
-    casper.log('STARTING TO SCRAPE...');
+    MARKETS_TO_SCRAPE = getMarketsToScrape().slice();
+    DESIGNERS_TO_SCRAPE = getDesignersToScrape().slice();
 });
+
+casper.then(function() {
+    casper.echo("SCRAPING...\n")
+})
+
+casper.then(function () {
+    printScrapeDetails();
+})
 
 casper.then(function () {
     loadFeed(NUMSCROLLS);
@@ -29,7 +36,61 @@ casper.then(function () {
 });
 
 casper.then(function () {
-    casper.log('FINISHED SCRAPING!');
+    this.echo('\nFINISHED!');
 });
 
 casper.run();
+
+function printScrapeDetails() {
+    if (MARKETS_TO_SCRAPE.length === 0) {
+        casper.echo("  MARKETS: ALL");
+    } else {
+        casper.echo("  MARKETS: " + MARKETS_TO_SCRAPE);
+    }
+
+    if (DESIGNERS_TO_SCRAPE.length === 0) {
+        casper.echo("  DESIGNERS: ALL");
+    } else {
+        casper.echo("  DESIGNERS: " + DESIGNERS_TO_SCRAPE);
+    }
+}
+
+function loadFeed (numScrolls) {
+    casper.repeat(numScrolls, function () {
+        casper.scrollToBottom();
+        casper.wait(1000, function () {
+            casper.log('SCROLL: ' + ++scrollNum);
+        });
+    })
+}
+
+function getMarketsToScrape() {
+    var result = [];
+
+    MARKETS.forEach(function (marketName) {
+        if (casper.cli.has(marketName)) {
+            result.push(marketName);
+        }
+    })
+
+    return result;
+}
+
+function getDesignersToScrape() {
+    if (casper.cli.has('designers')) {
+        var designers = casper.cli.get('designers').split(',');
+
+        designers.filter(function (designer) {
+            return designer.trim().length > 0;
+        });
+
+        designers.map(function (designer) {
+            return designer.toLowerCase();
+        });
+
+        return designers;
+    }
+
+    // Empty represents all designers
+    return [];
+}
