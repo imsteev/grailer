400b1345ca70ec5303a2ed75f3f6265b48067262
Stephen Chung <spchung@andrew.cmu.edu>
2017-08-06 13:55:31 -0400

    update package.json commands

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-var casper = require('casper').create();
 var fs = require('fs');
+var casper = require('casper').create({
+    logLevel: 'debug'
+});
 
 var NUMSCROLLS = 250
+var scrollNum = 0
 
 var loadFeed = function (numScrolls) {
     casper.repeat(numScrolls, function () {
         casper.scrollToBottom();
         casper.wait(1000, function () {
-            'scrolled'
+            casper.log('SCROLL: ' + ++scrollNum);
         });
     })
 }
 
 casper.start('https://grailed.com/', function() {
-
+    casper.log('STARTING TO SCRAPE...');
 });
 
-casper.then(function() {
+casper.then(function () {
     loadFeed(NUMSCROLLS);
 });
 
 casper.then(function () {
     var html = this.getHTML();
     fs.write('feed.html', html)
+});
+
+casper.then(function () {
+    casper.log('FINISHED SCRAPING!');
 });
 
 casper.run();
