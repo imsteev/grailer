f300cd55c687c9134a14307e15c6af66a0d205dd
Stephen Chung <spchung@andrew.cmu.edu>
2018-05-14 13:48:53 -0400

    rename to scrape.js

--- scrape.js
+++ scrape.js
@@ -18,13 +18,17 @@ var filter = new gf.GrailedFilter();
 var grailedSelectors = new gs.GrailedSelectors();
 
 // Start a new Casper instance connected to grailed.com
-casper.start("https://grailed.com/", function() {
+casper.start("https://grailed.com/", function () {
   MARKETS_TO_SCRAPE = getListFromCLI("markets", MARKETS).slice();
   DESIGNERS_TO_SCRAPE = getListFromCLI("designers", []).slice();
   LOCATIONS_TO_SCRAPE = getListFromCLI("locations", []).slice();
 });
 
-casper.then(function() {
+function configureFiltersFromObj(config) {
+  casper.sendKeys(grailedSelectors.search["query-input"], config.query);
+}
+
+casper.then(function () {
   if (casper.cli.has("q")) {
     var q = casper.cli.raw.get("q");
     casper.sendKeys(grailedSelectors.search["query-input"], q);
@@ -32,29 +36,37 @@ casper.then(function() {
   }
 });
 
+casper.then(function () {
+  if (casper.cli.has('configFile')) {
+    var obj = casper.cli.get("configFile");
+    var data = JSON.parse(fs.read(obj));
+    console.log(JSON.stringify(data));
+  }
+})
+
 // Grab categorical filters from command line
-casper.then(function() {
+casper.then(function () {
   configureCategoricalFilter("categories");
   configureCategoricalFilter("sizes");
 });
 
 // Click on selectors associated with the categorical filters
-casper.then(function() {
+casper.then(function () {
   clickSelectors(CATEGORY_PANEL_SELECTORS);
   clickSelectors(CATEGORY_SELECTORS);
 });
 
 // Click on location filters
-casper.then(function() {
+casper.then(function () {
   var locationSelectors = [];
-  LOCATIONS_TO_SCRAPE.forEach(function(location, _) {
+  LOCATIONS_TO_SCRAPE.forEach(function (location, _) {
     locationSelectors.push(grailedSelectors.locations[location]);
   });
   clickSelectors(locationSelectors);
 });
 
 // Set min/max price filters
-casper.then(function() {
+casper.then(function () {
   if (casper.cli.has("min")) {
     // https://stackoverflow.com/a/25014609/8109239
     minPrice = casper.cli.raw.get("min");
@@ -73,24 +85,24 @@ casper.then(function() {
 });
 
 // Click on sort filter
-casper.then(function() {
+casper.then(function () {
   configureSortFilter();
 });
 
 // Click on market filters
-casper.then(function() {
+casper.then(function () {
   configureMarketFilters();
 });
 
 // Search and click for designer filter
-casper.then(function() {
+casper.then(function () {
   var i = 0;
-  casper.repeat(DESIGNERS_TO_SCRAPE.length, function() {
+  casper.repeat(DESIGNERS_TO_SCRAPE.length, function () {
     clickDesignerFilter(DESIGNERS_TO_SCRAPE[i++]);
   });
 });
 
-casper.then(function() {
+casper.then(function () {
   if (casper.cli.has("numItems")) {
     try {
       var numItems = parseInt(casper.cli.get("numItems"));
@@ -102,18 +114,18 @@ casper.then(function() {
 });
 
 // Determine how many items should be scraped
-casper.then(function() {
+casper.then(function () {
   if (NUM_ITEMS !== 0 || DESIGNERS_TO_SCRAPE.length === 0) {
     return;
   }
-  MARKETS_TO_SCRAPE.forEach(function(marketName) {
+  MARKETS_TO_SCRAPE.forEach(function (marketName) {
     NUM_ITEMS += getMarketItemCount(marketName);
     casper.wait(500);
   });
 });
 
 // Scroll to load a number of items equal to NUM_ITEMS
-casper.then(function() {
+casper.then(function () {
   printFilterDetails();
   casper.echo("[SCRAPE DETAILS]\n");
   if (numFeedItems() > 0) {
@@ -124,13 +136,13 @@ casper.then(function() {
 });
 
 // Write the loaded content into a local file
-casper.then(function() {
+casper.then(function () {
   var html = this.getHTML(".feed", true);
   dest = casper.cli.has("f") ? casper.cli.get("f") : "./feed.html";
   fs.write(dest, html);
 });
 
-casper.then(function() {
+casper.then(function () {
   filter.add({
     markets: MARKETS_TO_SCRAPE,
     locations: LOCATIONS_TO_SCRAPE,
@@ -138,7 +150,7 @@ casper.then(function() {
   });
 });
 
-casper.then(function() {
+casper.then(function () {
   this.echo("\n[FINISHED]");
   if (numFeedItems() > 0) {
     this.echo("\n  TOTAL ITEMS SCRAPED: " + numFeedItems());
@@ -151,9 +163,26 @@ casper.then(function() {
 
 casper.run();
 
+function loadConfigFromCLI() {
+  if (casper.cli.has("q")) {
+    var q = casper.cli.raw.get("q");
+    casper.sendKeys(grailedSelectors.search["query-input"], q);
+    filter.add({ query: q });
+  }
+  configureCategoricalFilter("categories");
+  configureCategoricalFilter("sizes");
+  if (casper.cli.has("min")) {
+    // https://stackoverflow.com/a/25014609/8109239
+    minPrice = casper.cli.raw.get("min");
+  }
+  if (casper.cli.has("max")) {
+    maxPrice = casper.cli.raw.get("max");
+  }
+}
+
 function clickSelectors(selectors) {
   var i = 0;
-  casper.repeat(selectors.length, function() {
+  casper.repeat(selectors.length, function () {
     var selector = selectors[i++];
     casper.click(selector);
     casper.wait(200);
@@ -165,7 +194,7 @@ function clickDesignerFilter(designer) {
   casper.sendKeys(grailedSelectors.search["designer-input"], designer, {
     reset: true
   });
-  casper.wait(3000, function() {
+  casper.wait(3000, function () {
     try {
       var selector = grailedSelectors.search["designer-results"];
       casper.click(selector);
@@ -184,7 +213,7 @@ function clickDesignerFilter(designer) {
 }
 
 function numFeedItems() {
-  return casper.evaluate(function() {
+  return casper.evaluate(function () {
     return $("div.feed-item").length;
   });
 }
@@ -205,9 +234,9 @@ function loadFeedItems(prevFeedItemCount, numItems) {
     TRIES = 0;
   }
 
-  casper.then(function() {
+  casper.then(function () {
     casper.scrollToBottom();
-    casper.wait(1000, function() {
+    casper.wait(1000, function () {
       if (numFeedItems() < numItems && TRIES < TRY_SCROLL_LIMIT) {
         loadFeedItems(prevFeedItemCount, numItems);
       }
@@ -226,7 +255,7 @@ function setMarketFilter(selector, active) {
 
 function configureMarketFilters() {
   var i = 0;
-  casper.repeat(MARKETS.length, function() {
+  casper.repeat(MARKETS.length, function () {
     var marketName = MARKETS[i++];
     if (MARKETS_TO_SCRAPE.indexOf(marketName) == -1) {
       // By default, grails market is checked
@@ -252,7 +281,7 @@ function configureCategoricalFilter(domain) {
       CATEGORY_PANEL_SELECTORS.push(
         grailedSelectors[domain][categoryName]["panel"]
       );
-      subcategories.forEach(function(subcategory, _) {
+      subcategories.forEach(function (subcategory, _) {
         CATEGORY_SELECTORS.push(
           grailedSelectors[domain][categoryName][subcategory]
         );
@@ -265,10 +294,10 @@ function getListFromCLI(cliArg, L) {
   if (casper.cli.has(cliArg)) {
     var res = casper.cli.get(cliArg).split(",");
     return res
-      .map(function(item) {
+      .map(function (item) {
         return item.trim();
       })
-      .filter(function(item) {
+      .filter(function (item) {
         return item.length > 0;
       });
   }
@@ -292,13 +321,13 @@ function configureSortFilter() {
 }
 
 function printMarketFilterDetails() {
-  MARKETS.forEach(function(market, _) {
+  MARKETS.forEach(function (market, _) {
     require("utils").dump(
       casper.getElementInfo(grailedSelectors.debug["markets"][market])["text"]
     );
     require("utils").dump(
       casper.getElementInfo(grailedSelectors.debug["markets"][market])[
-        "attributes"
+      "attributes"
       ]
     );
   });
@@ -308,5 +337,37 @@ function printFilterDetails() {
   casper.echo("[FILTERS]");
   casper.echo("");
   casper.echo(JSON.stringify(filter.config, null, "  "));
+  casper.echo(getSelectorsFromFilter(filter.config));
   casper.echo("");
+}
+
+// inspired from https://gist.github.com/penguinboy/762197
+var flattenObject = function (ob) {
+  var toReturn = {};
+
+  for (var i in ob) {
+    if (!ob.hasOwnProperty(i)) continue;
+
+    if (typeof ob[i] == "object") {
+      var flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue;
+
+        toReturn[i + "." + flatObject[x]] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+};
+
+function getSelectorsFromFilter(filter) {
+  var flattened = flattenObject(filter.config);
+  for (var path in filter.config) {
+    for (var subpath in path.split(".")) {
+      console.log(subpath, ",");
+    }
+    console.log("---------");
+  }
 }
